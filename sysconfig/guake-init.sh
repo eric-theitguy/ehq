edbd7623f3e7ea6fb76b841bdcd2f38b5aed13a2
Eric Hendricks <eric@theitguys.us>
2018-11-11 18:52:22 -0600

    Taking a break from Kube

--- sysconfig/guake-init.sh
+++ sysconfig/guake-init.sh
@@ -6,4 +6,4 @@ guake -r 'local';
 # Create new tabs
 guake -n ~/ -r "joplin" -e '/usr/bin/joplin';
 guake -n ~/ -r "itg" -e "ssh -t itg 'tmux a'";
-guake -n ~/ -r "kube" -e "kubectl get pods";
+#guake -n ~/ -r "kube" -e "kubectl get pods";
