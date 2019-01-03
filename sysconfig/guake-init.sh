60befdb826a9aaa2830f277e5a15647e438a01e9
Eric Hendricks <eric@theitguys.us>
2019-01-03 17:55:54 -0600

    Removed LA Tab

--- sysconfig/guake-init.sh
+++ sysconfig/guake-init.sh
@@ -6,5 +6,4 @@ guake -r 'local';
 # Create new tabs
 guake -n ~/ -r "joplin" -e '/usr/bin/joplin';
 guake -n ~/ -r "itg" -e "ssh -t itg 'tmux a'";
-guake -n ~/ -r "LA";
 #guake -n ~/ -r "kube" -e "kubectl get pods";
